b8b6110a655d2fd0e6d1defd12590bf92df546bc
_FN10_ <mathdudex09@gmail.com>
2026-03-19 14:15:02 -0300

    change to not be depercated

--- src/main/java/fn10/psptools/util/SSLHelper.java
+++ src/main/java/fn10/psptools/util/SSLHelper.java
@@ -18,10 +18,10 @@ import java.security.cert.X509Certificate;
 public class SSLHelper {
 
     static public Connection getConnection(String url){
-        return Jsoup.connect(url).sslSocketFactory(SSLHelper.socketFactory());
+        return Jsoup.connect(url).sslContext(SSLHelper.socketFactory());
     }
 
-    static private SSLSocketFactory socketFactory() {
+    static private SSLContext socketFactory() {
         TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
             public java.security.cert.X509Certificate[] getAcceptedIssuers() {
                 return new X509Certificate[0];
@@ -37,9 +37,8 @@ public class SSLHelper {
         try {
             SSLContext sslContext = SSLContext.getInstance("SSL");
             sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            SSLSocketFactory result = sslContext.getSocketFactory();
 
-            return result;
+            return sslContext;
         } catch (NoSuchAlgorithmException | KeyManagementException e) {
             throw new RuntimeException("Failed to create a SSL socket factory", e);
         }
